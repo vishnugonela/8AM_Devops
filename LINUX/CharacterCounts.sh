eec044c6b37336dee1dd96f3c99a4215f6d50ab0
ImYogi07 <yogendrakumar.mavalluru007@gmail.com>
2022-12-18 15:43:43 +0530

    CharactersCount shell script file added in LINUX

---
+++ LINUX/CharacterCounts.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Shell Scripting for a Counting the Characters of a Sting
+# --------------------------------------------------------
+
+
+# string=$(echo $1 | tr '[:upper:]' '[:lower:]')
+string=$1
+read -p "Enter a String:" string
+
+for char in {a..z}
+do
+  count=`awk -F"${char}" '{print NF-1}' <<< "${string}"`
+  if [ $count  != 0 ]; then
+    echo -n $char:
+    echo $count
+  fi
+done
