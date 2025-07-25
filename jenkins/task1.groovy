27fb3ef1ade775bf7f5e4d8a7a8bf552f62bc3f2
Pooja614-cell <poojapatilj99@gmail.com>
2025-07-25 08:28:16 +0530

    Update task1.groovy

--- jenkins/task1.groovy
+++ jenkins/task1.groovy
@@ -1,37 +1,17 @@
-pipeline {
+pipeline{
+
     agent any
 
-    stages {
-        stage('List Jenkins Nodes') {
-            steps {
-                script {
-                    // Jenkins instance
-                    def jenkinsInstance = Jenkins.instance
+    stages{
+        stage(serverdetails){
+            steps{
+                sh '''
+    
+                     echo "server details"
 
-                    // Get all nodes (including master and agents)
-                    def nodes = jenkinsInstance.nodes
-
-                    // Print master details
-                    echo "Master Node:"
-                    def master = jenkinsInstance.getComputer("")
-                    echo "  Name: Master"
-                    echo "  Online: ${master.isOnline()}"
-                    echo "  Hostname: ${master.getHostName()}"
-                    echo "  Executors: ${master.countExecutors()}"
-
-                    // Print slave/agent details
-                    nodes.each { node ->
-                        def computer = node.toComputer()
-                        echo "Agent Node:"
-                        echo "  Name: ${node.name}"
-                        echo "  Online: ${computer.isOnline()}"
-                        echo "  Hostname: ${computer.getHostName()}"
-                        echo "  Executors: ${node.numExecutors}"
-                        echo "  Labels: ${node.getLabelString()}"
-                    }
-                }
+                     CPU details , `mpstat`
+                '''
             }
         }
     }
 }
-
