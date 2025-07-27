d2ae9e0c01fdac5901b8dd34ca06c3b20b162094
bhagirathi <n.bhagirathi@gmail.com>
2025-07-28 00:07:42 +0530

    adding multinode script

---
+++ multinode.groovy
@@ -0,0 +1,40 @@
+pipeline{
+agent any
+ stages{
+#First  stages on NODE1
+sh '''
+        stage('Stage 1 - NODE1') {
+            agent { label 'NODE1' }
+            steps {
+                echo "Running Stage 1 on NODE1"
+            }
+        }
+
+      stage('Stage 2-NODE1'){
+       agent { label 'NODE1'}
+
+        Steps{
+        echo "Running Stage2 on NODE1"
+     }
+
+   }
+    // Next 2 stages on NODE2
+        stage('Stage 6 - NODE2') {
+            agent { label 'NODE2' }
+            steps {
+                echo "Running Stage 6 on NODE2"
+            }
+        }
+        stage('Stage 7 - NODE2') {
+            agent { label 'NODE2' }
+            steps {
+                echo "Running Stage 7 on NODE2"
+            }
+        }
+
+      '''
+
+
+}
+
+}
