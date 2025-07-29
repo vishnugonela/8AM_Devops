2a865a07414b8ac14e4b15abc3dd76a7ab52e18d
Pooja614-cell <poojapatilj99@gmail.com>
2025-07-29 11:03:51 +0530

    Create task2.groovy

---
+++ jenkins/task2.groovy
@@ -0,0 +1,16 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Backup Jenkins Home') {
+            steps {
+                sh '''
+                    TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+                    BACKUP_DIR="/var/backups/jenkins"
+                    mkdir -p $BACKUP_DIR
+                    tar -czf $BACKUP_DIR/jenkins_backup_$TIMESTAMP.tar.gz -C $JENKINS_HOME .
+                '''
+            }
+        }
+    }
+}
