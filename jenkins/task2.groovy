a3978f745f514051400fea6e1d10ab92d4b7c8cb
Pooja614-cell <poojapatilj99@gmail.com>
2025-07-30 17:07:23 +0530

    Update task2.groovy

--- jenkins/task2.groovy
+++ jenkins/task2.groovy
@@ -7,7 +7,7 @@ pipeline {
                 sh '''
                     TIMESTAMP=$(date +%Y%m%d_%H%M%S)
                     BACKUP_DIR="/var/backups/jenkins"
-                    sudo mkdir -p $BACKUP_DIR
+                    sudo -S mkdir -p $BACKUP_DIR
                     tar -czf $BACKUP_DIR/jenkins_backup_$TIMESTAMP.tar.gz -C $JENKINS_HOME .
                 '''
             }
