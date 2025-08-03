32f7af7ce706e62b7ee3314bcb8ff2d5fc5df28e
bdabke <n.bhagirathi@gmail.com>
2025-08-03 19:31:21 +0530

    Update copyvarprev.groovy

--- jenkins/copyvarprev.groovy
+++ jenkins/copyvarprev.groovy
@@ -3,10 +3,10 @@ pipeline {
   stages {
     stage('Copy var from  prev job') {
       steps {
-       step{ [$class: 'CopyArtifact',
+       step([$class: 'CopyArtifact',
                     projectName: 'globalvar.groovy',
                     filter: 'filearch/credential.env',
-                    selector: [$class: 'LastSuccessfulBuildSelector']       ]}
+                    selector: [$class: 'LastSuccessfulBuildSelector']])
         
       }
     }
