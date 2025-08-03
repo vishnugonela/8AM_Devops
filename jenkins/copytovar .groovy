08e5b7a086950ee88714f4d980a23d6a194abec5
bdabke <n.bhagirathi@gmail.com>
2025-08-03 19:06:21 +0530

    Update copytovar .groovy

--- jenkins/copytovar .groovy
+++ jenkins/copytovar .groovy
@@ -1,26 +1,28 @@
-pipeline{
+pipeline {
   agent any
-  stages{
-    stage ( ' copyvar2 ')
-    {
-      steps{
+  stages {
+    stage('Copy var from  prev job') {
+      steps {
         copyArtifacts(
-          ProjectName:globalvar.groovy,
+          projectName: 'globalvar.groovy',
           selector: lastSuccessful(),
-          filter: 'filechar/credential.env'
-          )
-         }
-    }
-        stage('Use Variable') {
-            steps {
-                sh '''
-                    source filechar/credential.env
-                    echo "API_KEY: $API_KEY"
-                '''
-            }
-        }
+          filter: 'filearch/credential.env'
+        )
       }
     }
+
+    stage('Use Variable') {
+      steps {
+        sh '''
+          echo "Reading credential.env"
+          source filearch/credential.env
+          echo "USERNAME: $USERNAME"
+        '''
+      }
+    }
+  }
+}
+
      
           
           
