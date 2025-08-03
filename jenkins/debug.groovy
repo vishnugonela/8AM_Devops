1d9b9802b37e7d984492edd2e6e85ee1a0b6cbea
Pooja614-cell <poojapatilj99@gmail.com>
2025-08-03 07:40:47 +0530

    Update debug.groovy

--- jenkins/debug.groovy
+++ jenkins/debug.groovy
@@ -10,21 +10,23 @@ pipeline{
     stage('checkout'){
       steps{
         sh '''
-        [ "$DEBIG" = "true" ] && set -x
+        [ "$DEBUG" = "true" ] && set -x
         echo "Checking out code"
 
         '''
     }
 
-      stage('Build'){
+     
+        }
+      }
+
+   stage('Build'){
         steps{
           sh '''
           [ "$DEBUG" = "True" ] && set -x
           echo " Building a project "
 
           '''
-        }
-      }
     }
   }
   
