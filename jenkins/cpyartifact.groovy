6d7354743679cdae4ab53d09150797cb29e701e0
bdabke <n.bhagirathi@gmail.com>
2025-08-04 00:24:56 +0530

    Create cpyartifact.groovy

---
+++ jenkins/cpyartifact.groovy
@@ -0,0 +1,35 @@
+pipeline {
+  agent any
+
+  stages {
+    stage('Copy Artifacts from script-logger-job') {
+      steps {
+        copyArtifacts(
+          projectName: 'scriptlog',
+          selector: [$class: 'LastSuccessfulBuildSelector'],
+          filter: 'artifacts/*',
+          target: 'imported'
+        )
+      }
+    }
+
+    stage('Display .sh Log and Output') {
+      steps {
+        sh '''
+          echo "Imported files:"
+          ls -l imported/
+
+          echo "--- Content of runlog.sh ---"
+          cat imported/runlog.sh
+
+          echo "--- Re-running the script ---"
+          chmod +x imported/runlog.sh
+          ./imported/runlog.sh
+
+          echo "--- Original output.log from previous run ---"
+          cat imported/output.log
+        '''
+      }
+    }
+  }
+}
