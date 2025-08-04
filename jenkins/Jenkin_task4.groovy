5049540eb7bd176b39f1aef107c7b495e761e5a4
Santosh Sethy <santosh.sethy39@gmail.com>
2025-08-04 09:34:16 +0530

    Update Jenkin_task4.groovy

--- jenkins/Jenkin_task4.groovy
+++ jenkins/Jenkin_task4.groovy
@@ -5,17 +5,17 @@ pipeline {
         stage('Get User Input') {
             steps {
                 script {
-                   # Prompt the user to enter a username
-                      read -p "Enter your username (UPPERCASE, no spaces): " username
-
-                      # Validate the username: must be UPPERCASE letters only, no spaces
-                        if [[ "$username" =~ ^[A-Z]+$ ]]; then
-                          echo "Valid username: $username"
-                          else
-                echo "Invalid username. It must be UPPERCASE letters only with no spaces."
-                  fi
+                    def username = input(message: "Enter your username (UPPERCASE, no spaces):", parameters: [string(name: 'USERNAME')])
+                    if (username ==~ /^[A-Z]+$/) {
+                        echo "Valid username: ${username}"
+                    } else {
+                        error "Invalid username. It must be UPPERCASE letters only with no spaces."
+                    }
                 }
-                        stage('Continue Pipeline') {
+            }
+        }
+
+        stage('Continue Pipeline') {
             steps {
                 echo "Proceeding with further steps..."
             }
