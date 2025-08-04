6c0d158917f0567edc5646309417fc5454233f44
Pooja614-cell <poojapatilj99@gmail.com>
2025-08-04 14:23:44 +0530

    Update cpyartifact.groovy

--- jenkins/cpyartifact.groovy
+++ jenkins/cpyartifact.groovy
@@ -5,7 +5,7 @@ pipeline {
     stage('Copy Artifacts from script-logger-job') {
       steps {
         copyArtifacts(
-          projectName: 'scriptlog',
+          projectName: 'globalvar.groovy',
           selector: [$class: 'LastSuccessfulBuildSelector'],
           filter: 'artifacts/*',
           target: 'imported'
